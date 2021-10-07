1fab251c550c1ede1e64059e8cb285d0bdfdf47b
geoff4fsm <87156765+geoff4fsm@users.noreply.github.com>
2021-10-06 22:29:28 -0400

    Add files via upload

--- W2HW1/Computer.java
+++ W2HW1/Computer.java
@@ -1,31 +1,38 @@
 package W2HW1;
 
 public class Computer {
+
+    // private variables defined
     private String compName;
     private int compYear;
     private double compCost;
-
+    // Computer parameterized constructor
     public Computer( String compName, int compYear, double compCost) {
         this.compName = compName;
         this.compYear = compYear;
         this.compCost = compCost;
     }
-
+    // get computer name
     public String getCompName() {
         return compName;
     }
+    // set computer name
     public void setCompName( String compName ) {
         this.compName = compName;
     }
+    // get computer year
     public int getCompYear() {
         return compYear;
     }
+    // set computer year
     public void setCompYear( int compYear ) {
         this.compYear = compYear;
     }
+    // get computer cost
     public double getCompCost() {
         return compCost;
     }
+    // set computer cost
     public void setCompCost( double compCost ) {
         this.compCost = compCost;
     }
--- W2HW1/CreateComputer.java
+++ W2HW1/CreateComputer.java
@@ -4,6 +4,7 @@ import java.util.*;
 public class CreateComputer {
 
     public static void main(String[] args) {
+        // define variables
         String compName = null;
         int compYear = 0;
         double compCost = 0;
@@ -11,36 +12,53 @@ public class CreateComputer {
         int ans2;
 
         Scanner input = new Scanner(System.in);
+        // user input for adding a computer to computer array
         System.out.println("Would you like to add a computer? yes or no ");
         ans1 = input.nextLine();
+
         if ( ans1.equals("yes")) {
+            // user input for number of computers to add
             System.out.println("How many would you like to add? ");
             ans2 = input.nextInt();
+
+            // define number of computers to add to array
             Computer[] compArray = new Computer[ans2];
+
+            // loop for each computer to add to array
             for (int i = 0 ; i < ans2 ; i++) {
 
+                // define computer array with parameters
                 compArray[i] = new Computer(compName, compYear, compCost);
 
+                // user inputs computer name
                 System.out.println("Enter Computer Name: ");
                 input.nextLine();
                 compName = input.nextLine();
+
+                // set name to array
                 compArray[i].setCompName(compName);
 
+                // user inputs computer year
                 System.out.println("Enter Computer Year: ");
                 compYear = input.nextInt();
+
+                // set year to array
                 compArray[i].setCompYear(compYear);
 
+                // user inputs computer cost
                 System.out.println("Enter Computer Cost: ");
                 compCost = input.nextDouble();
+                // set cost to array
                 compArray[i].setCompCost(compCost);
             }
+            // loop to print each computer user enters
             for (int i = 0; i < ans2; i++) {
-                System.out.println("+++++++++++++++++++++++++++++");
+                System.out.println("++++++++++++++++++++++++++++++");
                 System.out.println("-  Computer Name: " + compArray[i].getCompName());
                 System.out.println("-  Computer Year: " + compArray[i].getCompYear());
                 System.out.printf("-  Computer Cost: $ " + "%.2f", compArray[i].getCompCost());
                 System.out.println();
-                System.out.println("+++++++++++++++++++++++++++++");
+                System.out.println("++++++++++++++++++++++++++++++");
             }
         } else System.out.println("Have a good day");
     }
